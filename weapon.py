60179fc9fbf085a0d9dcf4bfef0c0f43e79a95ce
3M-TinySpy <93517402+TinySpy01@users.noreply.github.com>
2022-10-04 09:59:21 +0100

    Add files via upload

--- weapon.py
+++ weapon.py
@@ -1,9 +1,15 @@
 class Weapon:
-    def __init__(self, ammunition: int, range:int):
-        self.ammuntion=ammunition
-        self.range=range
+    def __init__(self, ammunition: int, range: int):
+        self.ammuntion = ammunition
+        self.range = range
 
-    def _fire_at(self,x: int,y:int,z:int):
-        self.x=x
-        self.y=y
-        self.z=z
+    def _fire_at(self, x: int, y: int, z: int):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+
+Lance_missiles_antisurface = Weapon(40,30)
+Lance_missiles_anti_air = Weapon(50,40)
+Lance_torpilles = Weapon(15,20)
