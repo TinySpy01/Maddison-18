f4db4cb60ad3e8ef5c9c829857641731c650faa3
3M-TinySpy <93517402+TinySpy01@users.noreply.github.com>
2022-10-05 15:38:04 +0100

    Update weapon.py

--- weapon.py
+++ weapon.py
@@ -4,55 +4,41 @@ class Weapon:
         self.range = range
 
     def _fire_at(self, x: int, y: int, z: int):
-        if self.ammunition==0 : 
-            raise NoAmmunitionError("No Ammunition!")
-            
+        if self.ammunition == 0:
+            raise Exception("NoAmmunitionError")
+
 
 class Antisurface(Weapon):
     def __init__(self, ammunition: int, range: int):
-        super().__init__(ammunition,range)
-        
-        
-        
-    
+        super().__init__(ammunition, range)
+
     def _fire_at(self, x: int, y: int, z: int):
-        if self.ammunition==0 : 
-            raise NoAmmunitionError("No Ammunition!")
-        if z!=0 : 
-            self.ammunition-=1
-            raise OutOfRangeError("L'Antisurface ne marche qu'altitude nulle!")
-            
-          
+        if self.ammunition == 0:
+            raise Exception("NoAmmunitionError")
+        if z != 0:
+            self.ammunition -= 1
+            raise Exception("OutOfRangeError")
+
+
 class AntiAir(Weapon):
     def __init__(self, ammunition: int, range: int):
-        super().__init__(ammunition,range)
-        
-        
-        
-    
-    def _fire_at(self, x: int, y: int, z: int):
-        if self.ammunition==0 : 
-            raise NoAmmunitionError("No Ammunition!")
-        if z=<0 : 
-            self.ammunition-=1
-            raise OutOfRangeError("L'Antiair ne marche qu'altitude strictement positive!")
-            
-            
-class Lance-Torpilles(Weapon):
-    def __init__(self, ammunition: int, range: int):
-        super().__init__(ammunition,range)
-       
-        
-        
-    
-    def _fire_at(self, x: int, y: int, z: int):
-        if self.ammunition==0 : 
-            raise NoAmmunitionError("No Ammunition!")
-        if z>0 : 
-            self.ammunition-=1
-            raise OutOfRangeError("Le Lance-Torpilles ne marche qu'altitude négative!")
-                        
-            
-     
-                        
+        super().__init__(ammunition, range)
 
+    def _fire_at(self, x: int, y: int, z: int):
+        if self.ammunition == 0:
+            raise Exception("NoAmmunitionError")
+        if z= < 0:
+            self.ammunition -= 1
+            raise Exception("OutOfRangeError")
+
+
+class Lance_Torpilles(Weapon):
+    def __init__(self, ammunition: int, range: int):
+        super().__init__(ammunition, range)
+
+    def _fire_at(self, x: int, y: int, z: int):
+        if self.ammunition == 0:
+            raise Exception("NoAmmunitionError")
+        if z > 0:
+            self.ammunition -= 1
+            raise Exception("OutOfRangeError")
