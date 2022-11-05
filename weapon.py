48a6ba9b779ff985e7bc0bdfdf8f23768cedf285
AymSal77 <114984281+AymSal77@users.noreply.github.com>
2022-11-05 11:20:49 +0100

    Update weapon.py

--- weapon.py
+++ weapon.py
@@ -1,44 +1,80 @@
+from math import sqrt
+
+#Définir les exceptions
+
+class NoAmmunitionError(Exception):
+    pass
+class OutOfRangeError(Exception):
+    pass
+
+
 class Weapon:
     def __init__(self, ammunition: int, range: int):
-        self.ammuntion = ammunition
+        self.ammunition = ammunition
         self.range = range
 
-    def _fire_at(self, x: int, y: int, z: int):
+    def fire_at(self, x, y, z):
         if self.ammunition == 0:
-            raise Exception("NoAmmunitionError")
+            raise NoAmmunitionError
+        self.ammunition -= 1
+        if sqrt(x ** 2 + y ** 2) > self.range:
+            raise OutOfRangeError
 
+
+
+# Définir les armes
 
 class Antisurface(Weapon):
-    def __init__(self, ammunition: int, range: int):
-        super().__init__(ammunition, range)
-
-    def _fire_at(self, x: int, y: int, z: int):
-        if self.ammunition == 0:
-            raise Exception("NoAmmunitionError")
+    def __init__(self) -> None:
+        super().__init__(40, 30)
+    def fire_at(self,x, y, z):
+        super().fire_at(x,y,z)
         if z != 0:
-            self.ammunition -= 1
-            raise Exception("OutOfRangeError")
+            raise OutOfRangeError
 
 
 class AntiAir(Weapon):
-    def __init__(self, ammunition: int, range: int):
-        super().__init__(ammunition, range)
 
-    def _fire_at(self, x: int, y: int, z: int):
-        if self.ammunition == 0:
-            raise Exception("NoAmmunitionError")
-        if z= < 0:
-            self.ammunition -= 1
-            raise Exception("OutOfRangeError")
+    def __init__(self) -> None:
+        super().__init__(50, 40)
+    def fire_at(self,x, y,z):
+        super().fire_at(x,y,z)
+        if z <= 0:
+            raise OutOfRangeError
+
 
 
 class Lance_Torpilles(Weapon):
-    def __init__(self, ammunition: int, range: int):
-        super().__init__(ammunition, range)
-
-    def _fire_at(self, x: int, y: int, z: int):
-        if self.ammunition == 0:
-            raise Exception("NoAmmunitionError")
+    def __init__(self) -> None:
+        super().__init__(15, 20)
+    def fire_at(self,x, y,z):
+        super().fire_at(x,y,z)
         if z > 0:
-            self.ammunition -= 1
-            raise Exception("OutOfRangeError")
+            raise OutOfRangeError
+
+
+# Test unitaire:
+if __name__ == "__main__":
+    AntiAir = AntiAir()
+    Antisurface = Antisurface()
+    Lance_Torpilles= Lance_Torpilles()
+    Antisurface.fire_at(0,0,0)
+    assert Antisurface.ammunition == 39
+    try:
+        Antisurface.fire_at(0,0,1)
+    except:
+        assert Antisurface.ammunition == 38
+    AntiAir.fire_at(0,0,1)
+    assert AntiAir.ammunition == 49
+    try:
+        AntiAir.fire_at(0,0,0)
+    except:
+        assert AntiAir.ammunition == 48
+    Lance_Torpilles.fire_at(0,0,0)
+    assert Lance_Torpilles.ammunition == 14
+    Lance_Torpilles.fire_at(0,0,-1)
+    assert Lance_Torpilles.ammunition == 13
+    try:
+        Lance_Torpilles.fire_at(0,0,1)
+    except:
+        assert Lance_Torpilles.ammunition == 12
